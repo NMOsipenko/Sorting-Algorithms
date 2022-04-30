8c08def976fc8a78dc45e0da693fe6fae6d504b1
xBasTeRx <nmosipenko@mail.ru>
2022-04-30 17:24:45 +0300

    Update test

--- src/test/java/com/nmosipenko/study/sortingalgorithms/SortingAlgorithmsApplicationTests.java
+++ src/test/java/com/nmosipenko/study/sortingalgorithms/SortingAlgorithmsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nmosipenko.study.sortingalgorithms;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SortingAlgorithmsApplicationTests {
 
+	@Autowired
+	private Controller controller;
+
 	@Test
 	void contextLoads() {
+		Assert.assertNotNull(controller);
 	}
 
 }
