94376a1c74e1a39d8aea292debbad060b80354da
xBasTeRx <nmosipenko@mail.ru>
2022-05-09 15:45:35 +0300

    Add Selection sort

---
+++ src/main/java/com/nmosipenko/study/sortingalgorithms/services/ISortingService.java
@@ -0,0 +1,15 @@
+package com.nmosipenko.study.sortingalgorithms.services;
+
+import java.util.List;
+
+/**
+ * @author Osipenko-NM
+ * mail NMOsipenko@mail.ru
+ * @link https://github.com/NMOsipenko
+ */
+
+public interface ISortingService <T> {
+
+    List<T> sort ();
+
+}
