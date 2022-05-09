193f31e17190313ee257d1c4771ec3cb781142f2
xBasTeRx <nmosipenko@mail.ru>
2022-05-10 00:23:38 +0300

    Add Counting sort

---
+++ src/main/java/com/nmosipenko/study/sortingalgorithms/sorting/CountingSort.java
@@ -0,0 +1,17 @@
+package com.nmosipenko.study.sortingalgorithms.sorting;
+
+import java.util.List;
+
+/**
+ * @author Osipenko-NM
+ * mail NMOsipenko@mail.ru
+ * @link https://github.com/NMOsipenko
+ */
+
+public class CountingSort implements ISort<Integer>{
+
+ @Override
+ public List<Integer> sort(List<Integer> sortList) {
+  return sortList;
+ }
+}
