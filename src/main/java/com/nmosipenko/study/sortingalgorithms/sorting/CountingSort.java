c0deb8b89501bd9d45c569fc0a43cc84329836c5
xBasTeRx <nmosipenko@mail.ru>
2022-05-10 00:42:33 +0300

    Add Counting sort

--- src/main/java/com/nmosipenko/study/sortingalgorithms/sorting/CountingSort.java
+++ src/main/java/com/nmosipenko/study/sortingalgorithms/sorting/CountingSort.java
@@ -1,5 +1,6 @@
 package com.nmosipenko.study.sortingalgorithms.sorting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +9,44 @@ import java.util.List;
  * @link https://github.com/NMOsipenko
  */
 
-public class CountingSort implements ISort<Integer>{
+public class CountingSort implements ISort<Integer> {
 
- @Override
- public List<Integer> sort(List<Integer> sortList) {
-  return sortList;
- }
+    @Override
+    public List<Integer> sort(List<Integer> sortList) {
+
+        int minimum = sortList.get(0);
+        int maximum = sortList.get(0);
+
+        for (int i = 0; i < sortList.size(); i++) {
+
+            if (minimum > sortList.get(i)) {
+                minimum = sortList.get(i);
+            }
+
+            if (maximum < sortList.get(i)) {
+                maximum = sortList.get(i);
+            }
+
+        }
+
+        int[] countingArr= new int[(maximum - minimum + 1)];
+
+        for (int i = 0; i < sortList.size(); i++) {
+            Integer count = (sortList.get(i) - minimum);
+            System.out.println(count);
+            countingArr[count] = countingArr[count] + 1;
+        }
+
+        sortList.clear();
+
+        for (int i = 0; i < countingArr.length; i++) {
+            int count = countingArr[i];
+            for (int j = 0; j < count; j++) {
+                sortList.add(i + minimum);
+            }
+        }
+
+
+        return sortList;
+    }
 }
